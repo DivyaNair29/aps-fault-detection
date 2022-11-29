acd7bb415bfaa2843bf685c42584ac01baad4813
Divya S <dnair1461992@gmail.com>
2022-11-29 19:08:02 +0530

    updated

---
+++ sensor/entity/config_entity.py
@@ -0,0 +1,18 @@
+
+class DataIngestionCofig:
+    pass
+
+class DataValidationConfig:
+    pass
+
+class DataTransformationConfig:
+    pass
+
+class ModelTrainerConfig:
+    pass
+
+class ModelEvaluationConfig:
+    pass
+
+class ModelPusherConfig:
+    pass
