60813c341733ce9c9606e2230f7bd2bd6b7656b1
Divya S <dnair1461992@gmail.com>
2022-11-29 11:42:18 +0530

    This is our first version of code.

---
+++ dumpdata.py
@@ -0,0 +1,22 @@
+import pymongo
+import pandas as pd
+import json
+
+client = pymongo.MongoClient("mongodb://localhost:27017")
+
+DATAFILE_PATH = '/config/workspace/aps_failure_training_set1.csv'
+DATABASE_NAME = "aps"
+COLLECTION_NAME = "sensor"
+
+if __name__ == "__main__":
+    df = pd.read_csv(DATAFILE_PATH)
+    print(f"Rows And Columns: {df.shape}")
+
+    # convert dataframe into json format to dump these record in mongodb
+    df.reset_index(drop = True, inplace = True)
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    #insert converted data to mongodb
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    
